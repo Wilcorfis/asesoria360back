72bd61ad50a69dfb97bc3e2679e0f428e512b044
Wilcorfis <wilmerandres505@gmail.com>
2024-10-13 15:02:43 -0500

    ajustes

--- src/main/java/com/proyecto/ppi/repository/ContactRepository.java
+++
@@ -1,7 +0,0 @@
-package com.proyecto.ppi.repository;
-
-import com.proyecto.ppi.entity.Contact;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ContactRepository extends CrudRepository<Contact,Integer> {
-}
